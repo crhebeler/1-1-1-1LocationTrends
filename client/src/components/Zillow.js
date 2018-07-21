b474c12eabf374f8a2d214db134932f24b83d74d
chrisheb1 <34869576+chrisheb1@users.noreply.github.com>
2018-07-21 11:40:03 -0400

    added axios packages and updated layout

--- client/src/components/Zillow.js
+++ client/src/components/Zillow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ZillowHead from './ZillowHead';
 import NavLink from './Navlink';
 import Footer from './Footer';
-
+import Iframe from 'react-iframe'; 
 
 const Zillow = (props) => {
   return (
@@ -11,9 +11,17 @@ const Zillow = (props) => {
 <ZillowHead /> 
 
 
+<Iframe url="https://maps.hometownlocator.com/map_V3.html?mode=county&state=FL"
+        width="850px"
+        height="650px"
+        id="myId"
+        className="FloridaInteractiveMap"
+        display="initial"
+        position="relative"
+        allowFullScreen/>
 
 
-<p> source-https://florida.hometownlocator.com/maps/ </p> 
+
 
 <Footer /> 
   
