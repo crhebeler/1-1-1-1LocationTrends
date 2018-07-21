37cc8fdab55410b37537070351cc506997481e19
chrisheb1 <34869576+chrisheb1@users.noreply.github.com>
2018-07-21 00:31:21 -0400

    added npm package IFrame and added Florida interactive map -now fully functional

--- client/src/components/Zillow.js
+++ client/src/components/Zillow.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ZillowHead from './ZillowHead';
 import NavLink from './Navlink';
 import Footer from './Footer';
+import Iframe from 'react-iframe'
 
 const Zillow = (props) => {
   return (
     <div>
    <NavLink /> 
 <ZillowHead /> 
-<h1> Zillow is working </h1> 
 
+<Iframe url="https://maps.hometownlocator.com/map_V3.html?mode=county&state=FL"
+        width="650px"
+        height="650px"
+        id="myId"
+        className="FloridaInteractiveMap"
+        display="initial"
+        position="relative"
+        allowFullScreen/>
+
+
+<p> source-https://florida.hometownlocator.com/maps/ </p> 
 
 <Footer /> 
   
