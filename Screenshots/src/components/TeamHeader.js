49b21b9819dd937b065a85620ccafd873813eaa2
chrisheb1 <34869576+chrisheb1@users.noreply.github.com>
2018-08-15 01:19:57 -0400

    added screenshots

---
+++ Screenshots/src/components/TeamHeader.js
@@ -0,0 +1,33 @@
+import React from 'react'; 
+// All Headers will be styled the same in this CSS file
+import {Jumbotron} from 'reactstrap'; 
+//import '../styles/Header.css': 
+
+import '../styles/Team.css';
+
+
+
+const TeamHeader = (props) => {
+    return (
+
+   
+   
+    
+   <Jumbotron> 
+
+        
+      
+     
+<h1 className="display-3" margin-top="40px">Meet the Team</h1>
+<h4 className="lead"> Giving you valuable insights on the current data trends by location</h4>
+
+</Jumbotron>
+  
+  
+        
+
+    );
+  };
+  
+  export default TeamHeader;
+  
