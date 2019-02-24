a7dc11ad3ac84755fe17d8532ef9ebe893e9b6ff
Christopher <christopher@localhost.localdomain>
2019-02-24 15:11:08 -0500

    Updated multiple security vunerablites in the node packages

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = (props) => {
 <footer>
     
          <br/>
-      <h5>Location Trends App  &copy; 2018</h5> 
+      <h5>Location Trends App  &copy; 2019</h5> 
          <h6> <em>  Used Data from Florida Department of Health </em> </h6>  
          
     </footer> 
